78f5220681f98a5f2ec0b9b3165e5ff50a301985
jongwooME <jongwoolee2014@u.northwestern.edu>
2015-04-21 17:28:27 -0500

    HW1
    From mac

---
+++ EECS 214 Assignment 1/Assignment 1/ArrayQueue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_1
+{
+    /// <summary>
+    /// A queue internally implemented as an array
+    /// </summary>
+    public class ArrayQueue : Queue
+    {
+        object[] data = new object[100];
+        int head = 0;
+        int tail = 0;
+        /// <summary>
+        /// Add object to end of queue
+        /// </summary>
+        /// <param name="o">object to add</param>
+        public override void Enqueue(object o)
+        {
+            if (!IsFull)
+            {
+                data[tail] = o;
+                tail = (tail + 1) % data.Length;
+            }
+            else throw new QueueFullException();
+        }
+
+        /// <summary>
+        /// Remove object from beginning of queue.
+        /// </summary>
+        /// <returns>Object at beginning of queue</returns>
+        public override object Dequeue()
+        {
+            if (!IsEmpty)
+            {
+                object item = data[head];
+                head = (head + 1) % data.Length;
+                return item;
+            }
+            else throw new QueueEmptyException();
+        }
+
+        /// <summary>
+        /// The number of elements in the queue.
+        /// </summary>
+        public override int Count
+        {
+            get {
+                if (tail >= head) return tail - head;
+                else return (100 - head) + tail;
+            }
+        }
+
+        /// <summary>
+        /// True if the queue is full and enqueuing of new elements is forbidden.
+        /// </summary>
+        public override bool IsFull
+        {
+            get {
+                return Count >= 100;
+            }
+        }
+    }
+}
---
+++ EECS 214 Assignment 1/Assignment 1/LinkedListQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_1
+{
+    public class LLQueue
+    {
+
+        public object value;
+        public LLQueue next;
+        public LLQueue(object v, LLQueue n)
+        {
+            value = v;
+            next = n;
+        }
+    }
+    /// <summary>
+    /// A queue internally implemented as a linked list of objects
+    /// </summary>
+    public class LinkedListQueue : Queue
+    {
+        
+        
+        LLQueue prevLLQ = null;
+        /// <summary>
+        /// Add object to end of queue
+        /// </summary>
+        /// <param name="o">object to add</param>
+        public override void Enqueue(object o)
+        {
+            LLQueue headLLQ = new LLQueue(o, prevLLQ);
+        }
+
+        /// <summary>
+        /// Remove object from beginning of queue.
+        /// </summary>
+        /// <returns>Object at beginning of queue</returns>
+        public override object Dequeue()
+        {
+            // Remove this line when you implement this mehtod
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The number of elements in the queue.
+        /// </summary>
+        public override int Count
+        {
+            get
+            {
+                // Remove this line when you fill this method in.
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// True if the queue is full and enqueuing of new elements is forbidden.
+        /// Note: LinkedListQueues can be grown to arbitrary length, and so can
+        /// never fill.
+        /// </summary>
+        public override bool IsFull
+        {
+            get
+            {
+                // Remove this line when you fill this method in.
+                throw new NotImplementedException();
+            }
+        }
+    
+public  object value { get; set; }
+private  LinkedListQueue.LLQueue next { get; set; }}
+}
