5c6445b64aaf62cd584f0263c8e46bccc797ae68
jongwooME <jongwoolee2014@u.northwestern.edu>
2015-04-30 14:57:24 -0500

    4/30 thursday

---
+++ EECS-214 Assignment 2/Interpreter/ListDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    public class ListDictionary : Dictionary
+    {
+        public string name { get; set; }
+        public object val { get; set; }
+        public ListDictionary()
+        {
+            ListDictionary child;
+            this.name  = null;
+            this.val = null;
+        }
+
+        public override void Store(string key, object value)
+        {
+            this.name = key;
+            this.val = value;
+
+        }
+
+        public override object Lookup(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            // Fill this in with something like
+            //for (var cell = start; cell != null; cell = cell.next)
+            //     yield return new KeyValuePair<string, object>(cell.Key, cell.Value);
+            // And remove this:
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
