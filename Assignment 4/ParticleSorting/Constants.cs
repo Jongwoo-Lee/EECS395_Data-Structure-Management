80f1660a20006f571bdec5ad3c92a5a13e96f711
jongwooME <jongwoolee2014@u.northwestern.edu>
2015-05-14 08:51:05 -0500

    hw4

---
+++ Assignment 4/ParticleSorting/Constants.cs
@@ -0,0 +1,78 @@
+﻿using OpenGL;
+using System;
+
+namespace ParticleSorting
+{
+    class Constants
+    {
+        private const float PARTICLE_TRANSPARENCY = 0.05f;
+
+        public static VBO<Vector3> square;
+        public static VBO<Vector4> squareColor;
+        public static VBO<int> squareElements;
+        public static VBO<Vector3> cube;
+        public static VBO<Vector4> cubeColor;
+        public static VBO<int> cubeElements;
+        public static VBO<Vector4> whiteHUDColor;
+        public static VBO<Vector4> redHUDColor;
+        public static VBO<Vector4> exceededHUDColor;
+        public static Texture defaultTexture;
+        public static Texture starTexture;
+        public static VBO<Vector2> UV;
+        public static VBO<Vector2> UVCube;
+
+        public static void initBuffers()
+        {
+            // Square information
+            square = new VBO<Vector3>(new Vector3[] { new Vector3(1, 1, 0), new Vector3(-1, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0) });
+            squareColor = new VBO<Vector4>(new Vector4[] { new Vector4(1, 1, 1, PARTICLE_TRANSPARENCY), new Vector4(0, 1, 1, PARTICLE_TRANSPARENCY), new Vector4(1, 0, 1, PARTICLE_TRANSPARENCY), new Vector4(1, 1, 1, PARTICLE_TRANSPARENCY) });
+            squareElements = new VBO<int>(new int[] { 0, 1, 2, 3 }, BufferTarget.ElementArrayBuffer);
+
+            // Cube information
+            Vector3[] points = {
+                new Vector3(1, 1, 1), 
+                new Vector3(-1, 1, 1), 
+                new Vector3(-1, -1, 1), 
+                new Vector3(1, -1, 1), 
+                new Vector3(1, -1, -1), 
+                new Vector3(1, 1, -1), 
+                new Vector3(-1, 1, -1), 
+                new Vector3(-1, -1, -1)};
+            cube = new VBO<Vector3>(points);
+            Vector4[] colors = new Vector4[8];
+            for (int i = 0; i < 8; i++)
+            {
+                colors[i].x = i & 4;
+                colors[i].y = i & 2;
+                colors[i].z = i & 1;
+                colors[i].w = 1;
+            }
+            cubeColor = new VBO<Vector4>(colors);
+            int[] indices = {0, 1, 2,   0, 2, 3,
+                             0, 3, 4,   0, 4, 5,
+                             0, 5, 6,   0, 6, 1,
+                             1, 6, 7,   1, 7, 2,
+                             7, 4, 3,   7, 3, 2,
+                             4, 7, 6,   4, 6, 5};
+            cubeElements = new VBO<int>(indices, BufferTarget.ElementArrayBuffer);
+
+            // HUD color information
+            whiteHUDColor = new VBO<Vector4>(new Vector4[] { new Vector4(1, 1, 1, 1), new Vector4(1, 1, 1, 1), new Vector4(1, 1, 1, 1), new Vector4(1, 1, 1, 1) });
+            redHUDColor = new VBO<Vector4>(new Vector4[] { new Vector4(1, 0.6, 0.6, 1), new Vector4(1, 0.6, 0.6, 1), new Vector4(1, 0.6, 0.6, 1), new Vector4(1, 0.6, 0.6, 1) });
+            exceededHUDColor = new VBO<Vector4>(new Vector4[] { new Vector4(1, 0, 0, 1), new Vector4(1, 0, 0, 1), new Vector4(1, 0, 0, 1), new Vector4(1, 0, 0, 1) });
+
+            // Load texture information
+            starTexture = new Texture("smoke.bmp");
+            defaultTexture = new Texture("default.bmp");
+            UV = new VBO<Vector2>(new Vector2[] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) });
+            UVCube = new VBO<Vector2>(new Vector2[] {
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1)
+            });
+        }
+    }
+}
---
+++ Assignment 4/ParticleSorting/DrawFunctions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenGL;
+
+namespace ParticleSorting
+{
+    class DrawFunctions
+    {
+        public static void DrawSquare(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.square, Constants.squareColor, Constants.squareElements, Constants.defaultTexture, Constants.UV, BeginMode.TriangleFan, 4, model, view, projection);
+        }
+
+        public static void DrawStar(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.square, Constants.squareColor, Constants.squareElements, Constants.starTexture, Constants.UV, BeginMode.TriangleFan, 4, model, view, projection);
+        }
+
+        public static void DrawColoredCube(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.cube, Constants.cubeColor, Constants.cubeElements, Constants.defaultTexture, Constants.UVCube, BeginMode.Triangles, 36, model, view, projection);
+        }
+
+        public static void DrawWhiteHUDFrame(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.square, Constants.whiteHUDColor, Constants.squareElements, Constants.defaultTexture, Constants.UV, BeginMode.LineLoop, 4, model, view, projection);
+        }
+
+        public static void DrawRedHUDFrame(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.square, Constants.redHUDColor, Constants.squareElements, Constants.defaultTexture, Constants.UV, BeginMode.TriangleFan, 4, model, view, projection);
+        }
+
+        public static void DrawRedderHUDFrame(Matrix4? model = null, Matrix4? view = null, Matrix4? projection = null)
+        {
+            DrawShape(Constants.square, Constants.exceededHUDColor, Constants.squareElements, Constants.defaultTexture, Constants.UV, BeginMode.TriangleFan, 4, model, view, projection);
+        }
+
+        public static void DrawShape(VBO<Vector3> pos, VBO<Vector4> color, VBO<int> elements, Texture texture, VBO<Vector2> UV, BeginMode mode, int n, Matrix4? model, Matrix4? view, Matrix4? projection)
+        {
+            // Sets optional parameters to default values if they were not provided
+            model = model ?? Matrix4.Identity;
+            view = view ?? Matrix4.LookAt(new Vector3(0, 0, 10f), Vector3.Zero, Vector3.Up);
+            projection = projection ?? Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)Program.width / Program.height, 0.1f, 1000f);
+
+            // Sends the matrices to the vertex shader
+            Program.shader.Use();
+            Program.shader["u_Model"].SetValue(model.Value);
+            Program.shader["u_View"].SetValue(view.Value);
+            Program.shader["u_Projection"].SetValue(projection.Value);
+
+            // Binds vertex and color arrays to the vertex shader attributes
+            Gl.BindBufferToShaderAttribute(pos, Program.shader, "a_Position");
+            Gl.BindBufferToShaderAttribute(color, Program.shader, "a_FragColor");
+            Gl.BindBufferToShaderAttribute(UV, Program.shader, "a_VertexUV");
+            Gl.BindTexture(texture);
+            Gl.BindBuffer(elements);
+            Gl.DrawElements(mode, n, DrawElementsType.UnsignedInt, IntPtr.Zero);
+        }
+    }
+}
